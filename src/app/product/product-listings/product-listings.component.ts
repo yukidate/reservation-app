75a192452ca0661c8bdda5f01b9fd3b9d9e06009
Yuki <datepp1025@gmail.com>
2023-12-31 21:35:18 +0900

    front and backend

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from 'src/app/products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-listings',
@@ -9,10 +9,18 @@ import { products } from 'src/app/products';
 export class ProductListComponent implements OnInit {
 
   products: any
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = products
+    const productObservable = this.productService.getProducts()
+    productObservable.subscribe(
+      (data) => {
+        this.products = data
+        console.log('get data' + data)
+      },
+      (err) => {console.log('error' + err)},
+      () => {console.log()}
+    )
   }
 
 }
